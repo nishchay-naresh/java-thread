68f43d9eb468a04fbc8817542259ee7b72993c7d
nnares <nishchay.naresh@gmail.com>
2020-10-18 19:18:23 +0530

    + updating review comment

--- src/com/nishchay/concurrentpkg/synchronizers/synchronousq/QueueConsumer.java
+++ src/com/nishchay/concurrentpkg/synchronizers/synchronousq/QueueConsumer.java
@@ -16,6 +16,10 @@ public class QueueConsumer implements Runnable {
             String event = queue.take();
             // thread will block here
             System.out.printf("[%s] consumed event : %s %n", Thread.currentThread().getName(), event);
+
+/*            event = queue.take();
+            // thread will block here
+            System.out.printf("[%s] consumed event : %s %n", Thread.currentThread().getName(), event);*/
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
