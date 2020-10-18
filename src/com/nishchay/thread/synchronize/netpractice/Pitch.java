92e99e943f14638b9e6c8d99cae3d7a3903faef2
nnares <nishchay.naresh@gmail.com>
2020-10-18 19:26:12 +0530

    + updating review comment

--- src/com/nishchay/thread/synchronize/netpractice/Pitch.java
+++ src/com/nishchay/thread/synchronize/netpractice/Pitch.java
@@ -1,8 +1,12 @@
 package com.nishchay.thread.synchronize.netpractice;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Pitch {
 
     // using synchronization keyword
+/*
     public void practice(String playerName) {
         synchronized (this) {
             for (int i = 1; i <= 5; i++) {
@@ -10,19 +14,22 @@ public class Pitch {
             }
         }
     }
-
-    /*
-class Pitch {
-    // using reentrant lock
-	Lock lock = new ReentrantLock();
-	public void practice(String name) {
-        lock.lock();
-		for (int i = 1; i <= 5; i++) {
-			 System.out.println(Thread.currentThread().getName() + " Currently practicing - " + name);
-		}
-        lock.unlock();
-	}
-}
 */
+
+
+
+	public void practice(String name) {
+        // using reentrant lock
+        Lock lock = new ReentrantLock();
+
+        lock.lock();
+        try{
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " Currently practicing - " + name);
+            }
+        }finally{
+            lock.unlock();
+        }
+	}
 
 }
