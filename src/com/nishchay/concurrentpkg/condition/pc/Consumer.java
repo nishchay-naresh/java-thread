db62e78dd351a87c2b655614caedad0f296d776a
nnares <nishchay.naresh@gmail.com>
2020-10-19 19:45:04 +0530

    + com.nishchay.concurrentpkg.condition

---
+++ src/com/nishchay/concurrentpkg/condition/pc/Consumer.java
@@ -0,0 +1,19 @@
+package com.nishchay.concurrentpkg.condition.pc;
+
+public class Consumer extends Thread {
+    ProducerConsumerImpl pc;
+
+    public Consumer(ProducerConsumerImpl sharedObject) {
+        super("CONSUMER");
+        this.pc = sharedObject;
+    }
+
+    @Override
+    public void run() {
+        try {
+            pc.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
