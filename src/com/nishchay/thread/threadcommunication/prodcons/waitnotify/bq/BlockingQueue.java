fb945f58640263ec76bc484b5691adb0a624d453
nnares <nishchay.naresh@gmail.com>
2020-11-23 21:17:52 +0530

    + prodcons.waitnotify.bq

---
+++ src/com/nishchay/thread/threadcommunication/prodcons/waitnotify/bq/BlockingQueue.java
@@ -0,0 +1,59 @@
+package com.nishchay.thread.threadcommunication.prodcons.waitnotify.bq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Designing a BlockingQueue using Lock & Condition
+ * Designed it for the generic data type
+ * Underlying data structure is LinkList
+ * */
+public class BlockingQueue<T> {
+
+    private List<T> dataList;
+    private int limit;
+
+    public BlockingQueue(int size) {
+        this.limit = size;
+        dataList = new ArrayList<>(limit);
+    }
+
+    public boolean isFull() {
+        return dataList.size() == limit;
+    }
+
+    public boolean isEmpty() {
+        return dataList.size() == 0;
+    }
+
+    public synchronized void put(T element) {
+
+        while (isFull()) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // Append specified element to the end of list.
+        dataList.add(element);
+        this.notify();
+    }
+
+    public synchronized T take() {
+
+        while (isEmpty()) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // picking up the element from start
+        T message = dataList.remove(0);
+        this.notify();
+        return message;
+    }
+
+}
---
+++ src/com/nishchay/thread/threadcommunication/prodcons/waitnotify/bq/Driver.java
@@ -0,0 +1,64 @@
+package com.nishchay.thread.threadcommunication.prodcons.waitnotify.bq;
+
+/**
+ * Created two static methods corresponds to producer task & consumer task
+ * then spawning two thread - prodThread, consThread using lambda exp
+ *
+ * */
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        final int BUFFER_SIZE = 3;
+        BlockingQueue<Integer> queue = new BlockingQueue<>(BUFFER_SIZE);
+
+        Thread prodThread = new Thread(() -> produce(queue), "Producer Thread");
+        prodThread.start();
+
+        Thread consThread = new Thread(() -> consume(queue), "Consumer Thread");
+        consThread.start();
+    }
+
+
+    public static void produce(BlockingQueue<Integer> bq) {
+        for (int i = 1; i <= 10; i++) {
+            bq.put(i);
+            System.out.println(Thread.currentThread().getName() + " produces: " + i);
+        }
+    }
+
+    public static void consume(BlockingQueue<Integer> bq) {
+        Integer data;
+        for (int i = 1; i <= 10; i++) {
+            data = bq.take();
+            System.out.println(Thread.currentThread().getName() + " consumes: " + data);
+        }
+    }
+}
+
+/*
+ *
+ * O/P =>
+ *    Producer Thread produces: 1
+ *    Producer Thread produces: 2
+ *    Producer Thread produces: 3
+ *    Consumer Thread consumes: 1
+ *    Consumer Thread consumes: 2
+ *    Consumer Thread consumes: 3
+ *    Producer Thread produces: 4
+ *    Producer Thread produces: 5
+ *    Producer Thread produces: 6
+ *    Consumer Thread consumes: 4
+ *    Consumer Thread consumes: 5
+ *    Consumer Thread consumes: 6
+ *    Producer Thread produces: 7
+ *    Producer Thread produces: 8
+ *    Producer Thread produces: 9
+ *    Consumer Thread consumes: 7
+ *    Consumer Thread consumes: 8
+ *    Consumer Thread consumes: 9
+ *    Producer Thread produces: 10
+ *    Consumer Thread consumes: 10
+ *
+* */
