8e5b7fcab8a52c3cb47f5bf15ce2d80555563ee6
nnares <nishchay.naresh@gmail.com>
2020-11-25 23:25:35 +0530

    +u CallableWithoutExecutor.java

--- src/com/nishchay/concurrentpkg/callable/CallableWithoutExecutor.java
+++ src/com/nishchay/concurrentpkg/callable/CallableWithoutExecutor.java
@@ -5,27 +5,27 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 /*
-* FutureTask is a concrete class that implements both Runnable and Future
-* bcus of FutureTask only able to submit a Callable instance to an Individual thread (Thread class instance)
-* Callable instance --> FutureTask instance --> Thread class instance
-*
-* */
+ * FutureTask is a concrete class that implements both Runnable and Future
+ * bcus of FutureTask only able to submit a Callable instance to an Individual thread (Thread class instance)
+ * Callable instance --> FutureTask instance --> Thread class instance
+ *
+ * */
 
 public class CallableWithoutExecutor {
 
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-		Callable<Integer> integerCallable = () -> {
-			Thread.sleep(2 * 1000);
+        Callable<Integer> integerCallable = () -> {
+            Thread.sleep(2 * 1000);
 			return 100;
-		};
+        };
 
-		// FutureTask is a concrete class that implements both Runnable and Future
-		FutureTask<Integer> futureTask = new FutureTask<>(integerCallable);
+        // FutureTask is a concrete class that implements both Runnable and Future
+        FutureTask<Integer> futureTask = new FutureTask<>(integerCallable);
 
-		Thread t = new Thread(futureTask);
-		t.start();
+        Thread t = new Thread(futureTask);
+        t.start();
 
-		System.out.println(futureTask.get());
-	}
+        System.out.println(futureTask.get());
+    }
 }
