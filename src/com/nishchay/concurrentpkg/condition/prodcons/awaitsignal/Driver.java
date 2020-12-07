db52cf6e75f711163d8933940075b68790d94675
nnares <nishchay.naresh@gmail.com>
2020-12-08 00:11:01 +0530

    + condition.prodcons.awaitsignal

---
+++ src/com/nishchay/concurrentpkg/condition/prodcons/awaitsignal/Driver.java
@@ -0,0 +1,34 @@
+package com.nishchay.concurrentpkg.condition.prodcons.awaitsignal;
+
+
+public class Driver {
+
+    public static final int LIMIT = 5;
+
+    public static void main(String[] args) {
+
+        SharedObject sharedObject = new SharedObject(true);
+
+        new Thread(() -> produce(sharedObject), "Producer Thread").start();
+        new Thread(() -> consume(sharedObject), "Consumer Thread").start();
+
+    }
+
+//    instead of creating a new class for thread implementation, putting thread logic in a method
+//    later invoking these method thought threads created using lambdas
+    public static void produce(SharedObject sharedObject) {
+        for (int i = 1; i <= LIMIT; i++) {
+            sharedObject.produce(i);
+            System.out.println(Thread.currentThread().getName() + " produces:" + i);
+        }
+    }
+
+    public static void consume(SharedObject sharedObject) {
+        int value;
+        for (int i = 1; i <= LIMIT; i++) {
+            value = sharedObject.consume();
+            System.out.println(Thread.currentThread().getName() + " consume:" + value);
+        }
+    }
+
+}
