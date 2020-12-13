e72afc7cdee6038154fe2636b71f6c9978fa44a9
nnares <nishchay.naresh@gmail.com>
2020-12-13 16:31:04 +0530

    +u AtomicEx.java

--- src/com/nishchay/concurrentpkg/atomic/AtomicEx.java
+++ src/com/nishchay/concurrentpkg/atomic/AtomicEx.java
@@ -2,16 +2,19 @@ package com.nishchay.concurrentpkg.atomic;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class AtomicEx {
 
     public static void main(String[] args) {
 
-        atomicIntegerDemo();
+//        atomicIntegerDemo();
+//        atomicReferenceDemo();
 
-        atomicReferenceDemo();
+        atomicStampedReferenceDemo();
 
     }
+
 
     public static void atomicIntegerDemo() {
 
@@ -78,4 +81,104 @@ public class AtomicEx {
         System.out.println("exchanged: " + exchanged); // false
 
     }
+
+    private static void atomicStampedReferenceDemo() {
+
+        // Creating a Typed AtomicStampedReference
+        String initialRef = null;
+        int initialStamp = 0;
+        AtomicStampedReference<String> atomicStampedReference = new AtomicStampedReference<>(initialRef, initialStamp);
+
+        // Getting the AtomicStampedReference Reference
+        // Getting the AtomicStampedReference Stamp
+        System.out.println("atomicStampedReference.getReference() = " + atomicStampedReference.getReference());
+        System.out.println("atomicStampedReference.getStamp() = " + atomicStampedReference.getStamp());
+
+
+        initialRef = "first text";
+        atomicStampedReference = new AtomicStampedReference<>(initialRef, 0);
+        System.out.println("atomicStampedReference.getReference() = " + atomicStampedReference.getReference());
+        System.out.println("atomicStampedReference.getStamp() = " + atomicStampedReference.getStamp());
+
+
+        // Getting Reference and Stamp Atomically
+        initialRef = "text";
+        initialStamp = 1;
+
+        atomicStampedReference = new AtomicStampedReference<>(initialRef, initialStamp);
+
+        int[] stampHolder = new int[1];
+        String ref = atomicStampedReference.get(stampHolder);
+        System.out.println("ref = " + ref);
+        System.out.println("stamp = " + stampHolder[0]);
+
+        // Setting the AtomicStampedReference Reference
+        atomicStampedReference = new AtomicStampedReference<>(null, 0);
+        String newRef = "New object referenced";
+        int newStamp = 1;
+
+        atomicStampedReference.set(newRef, newStamp);
+        System.out.println("atomicStampedReference.getReference() = " + atomicStampedReference.getReference());
+        System.out.println("atomicStampedReference.getStamp() = " + atomicStampedReference.getStamp());
+
+        //---------Comparing and Setting the AtomicStampedReference Reference
+
+        initialRef = "initial value referenced";
+        initialStamp = 0;
+
+        AtomicStampedReference<String> atomicStringReference = new AtomicStampedReference<>(initialRef, initialStamp);
+
+        newRef = "new value referenced";
+        newStamp = initialStamp + 1;
+
+        boolean exchanged = atomicStringReference
+                .compareAndSet(initialRef, newRef, initialStamp, newStamp);
+        System.out.println("exchanged: " + exchanged);  //true
+
+        exchanged = atomicStringReference
+                .compareAndSet(initialRef, "new string", newStamp, newStamp + 1);
+        System.out.println("exchanged: " + exchanged);  //false
+
+        exchanged = atomicStringReference
+                .compareAndSet(newRef, "new string", initialStamp, newStamp + 1);
+        System.out.println("exchanged: " + exchanged);  //false
+
+        exchanged = atomicStringReference
+                .compareAndSet(newRef, "new string", newStamp, newStamp + 1);
+        System.out.println("exchanged: " + exchanged);  //true
+
+
+        // ----------------------- The code below shows how to detect the A-B-A situation using the AtomicStampedReference:
+/*
+        int[] stampHolder = new int[1];
+        Object ref = atomicStampedReference.get(stampHolder);
+
+        if(ref == null){
+            //prepare optimistic modification
+        }
+
+        //if another thread changes the
+        //reference and stamp here,
+        //it can be detected
+
+        int[] stampHolder2 = new int[1];
+        Object ref2 = atomicStampedReference.get(stampHolder);
+
+        if(ref == ref2 && stampHolder[0] == stampHolder2[0]){
+            //no modification since optimistic modification was prepared
+
+        } else {
+            //retry from scratch.
+        }
+*/
+
+    }
+
+    // Getting the AtomicStampedReference Reference
+    // Getting the AtomicStampedReference Stamp
+    private static void atomicStampedReferenceDemo1() {
+
+    }
+
+
 }
