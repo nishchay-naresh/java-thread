27b156e6f3c5c22fa7a797648e173aa685d84c02
nnares <nishchay.naresh@gmail.com>
2020-12-30 13:09:06 +0530

    +u GetCMEDemo2Threads.java

--- src/com/nishchay/concurrentpkg/collection/GetCMEDemo2Threads.java
+++ src/com/nishchay/concurrentpkg/collection/GetCMEDemo2Threads.java
@@ -3,31 +3,31 @@ package com.nishchay.concurrentpkg.collection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GetCMEDemo2Threads {
 
     public static void main(String[] args) throws InterruptedException {
 
         List<String> list = new ArrayList<>();
+//        List<String> list = new CopyOnWriteArrayList<>(); // safe against ConcurrentModificationException
         list.add("java");
         list.add("go");
         list.add("perl");
         list.add("ruby");
         System.out.println("original list - " + list);
         // Exception in thread "main" java.util.ConcurrentModificationException
-        // Iterator<String> itr = list.iterator();
+         Iterator<String> itr = list.iterator(); // iterator should assign prior to structural change
 
         Thread t = new Thread(() -> addToList(list));
         t.start();
         t.join();
 
-        Iterator<String> itr = list.iterator();
+//        Iterator<String> itr = list.iterator();
         while (itr.hasNext()) {
             String item = itr.next();
             System.out.println(item);
         }
-
-
         System.out.println("changed list - " + list);
     }
 
