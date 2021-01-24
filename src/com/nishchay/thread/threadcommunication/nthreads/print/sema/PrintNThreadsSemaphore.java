b35f0f9022e2186ae8f6f0a3d2a1f8ceaaac74ce
nnares <nishchay.naresh@gmail.com>
2021-01-24 13:46:49 +0530

    +u thread.threadcommunication.nthreads

--- src/com/nishchay/thread/threadcommunication/nthreads/print/sema/PrintNThreadsSemaphore.java
+++ src/com/nishchay/thread/threadcommunication/nthreads/print/sema/PrintNThreadsSemaphore.java
@@ -2,6 +2,15 @@ package com.nishchay.thread.threadcommunication.nthreads.print.sema;
 
 import java.util.concurrent.Semaphore;
 
+/*
+ *
+ * Print 1-N using some no of threads(here 4 again should be configurable)
+ * 	Thread-1 : 1, 1, 1, 1, 1
+ * 	Thread-2 : 2, 2, 2, 2, 2
+ * 	Thread-3 : 3, 3, 3, 3, 3
+ * 	Thread-4 : 4, 4, 4, 4, 4
+ *
+ * */
 public class PrintNThreadsSemaphore {
 
     public static void main(String[] args) {
