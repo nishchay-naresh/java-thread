7cfe6dab09f2e2eb521d54c1b0db6705a5691aee
nnares <nishchay.naresh@gmail.com>
2021-06-24 10:02:48 +0530

    +u

--- src/com/nishchay/thread/basic/yield/YieldDemo.java
+++ src/com/nishchay/thread/basic/yield/YieldDemo.java
@@ -1,7 +1,7 @@
 package com.nishchay.thread.basic.yield;
 
 /*
- * Thread.yield();  -  only method who doesnt throw checked exception - InterruptedException
+ * Thread.yield();  -  only method who doesn't throw checked exception - InterruptedException
  * stopping current execution thread and giving a chance to other threads to get the cpu
  */
 public class YieldDemo {
