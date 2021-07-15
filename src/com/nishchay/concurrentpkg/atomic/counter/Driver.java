2be8ee2b2af7f04b886d54ad110acaf80f65888c
nnares <nishchay.naresh@gmail.com>
2021-07-15 11:23:15 +0530

    +u concurrentpkg.atomic.counter

--- src/com/nishchay/concurrentpkg/atomic/counter/Driver.java
+++ src/com/nishchay/concurrentpkg/atomic/counter/Driver.java
@@ -8,22 +8,31 @@ public class Driver {
 
     public static void main(String[] args) throws InterruptedException {
 
-//        raceConditionExample();
-
         // Race Condition Example -  bcus of atomicity issue
-//        ICounter counter = new Counter();
+        // ICounter counter = new Counter();
+        /*
+        *  we will get inconsistent count for multiple run  bcus of atomicity issue
+        *   Final count is : 992
+        *   Final count is : 994
+        *   Final count is : 985
+        *   Final count is : 996
+        * we will ge the consistent count for rest all kind of counter
+        *   Final count is : 1000
+        *
+        * */
 
         // fixing atomicity issue - using Synchronization
-//        ICounter counter = new SynchronizedCounter();
+        // ICounter counter = new SynchronizedCounter();
 
         // fixing atomicity issue - using Lock
-//        ICounter counter = new CounterUsingLock();
+        // ICounter counter = new CounterUsingLock();
 
         // fixing atomicity issue - using atomic package
         ICounter counter = new AtomicCounter();
 
         hundredThreadIncrement(counter);
     }
+
 
     private static void hundredThreadIncrement(ICounter counter) throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(5);
