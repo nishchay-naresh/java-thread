9099fa5d5cc3ab3e8af19065325d7846192bc22f
nnares <nishchay.naresh@gmail.com>
2022-01-17 20:19:35 +0530

    +u thread.synchronijavaze.TimeBomb.java

--- src/com/nishchay/thread/synchronize/TimeBomb.java
+++ src/com/nishchay/thread/synchronize/TimeBomb.java
@@ -2,11 +2,28 @@ package com.nishchay.thread.synchronize;
 
 public class TimeBomb extends Thread {
 
-	private String[] timeStr = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-	private Object lock;
-	public boolean childDone;
+	public static void main(String[] s) throws InterruptedException {
 
-	public TimeBomb(Object lock) {
+		System.out.println("Starting 10 second count down... ");
+
+		Object lock = new Object();
+		TimeBomb timer = new TimeBomb(lock);
+		timer.start();
+
+		synchronized (lock) {
+			while (!timer.childDone) {
+				lock.wait();
+			}
+		}
+		System.out.println("Boom!!!");
+
+	}
+
+	private String[] timeStr = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+	private final Object lock;
+	private boolean childDone;
+
+	private TimeBomb(Object lock) {
 		super();
 		this.lock = lock;
 		this.childDone = false;
@@ -20,7 +37,7 @@ public class TimeBomb extends Thread {
 		}
 	}
 
-	public void countDown() {
+	private void countDown() {
 		for (int i = 9; i >= 0; i--) {
 			try {
 				System.out.println(timeStr[i]);
@@ -29,21 +46,6 @@ public class TimeBomb extends Thread {
 				ie.printStackTrace();
 			}
 		}
-	}
-
-	public static void main(String[] s) throws InterruptedException {
-		System.out.println("Starting 10 second count down... ");
-
-		Object lock = new Object();
-		TimeBomb timer = new TimeBomb(lock);
-		timer.start();
-
-		synchronized (lock) {
-			while (!timer.childDone) {
-				lock.wait();
-			}
-		}
-		System.out.println("Boom!!!");
 	}
 
 }
