279a3dd13e19b3a3e9fbf256768c784af05b74e2
nnares <nishchay.naresh@gmail.com>
2022-05-31 17:19:52 +0530

    +u thread.basic.join.Add100ThreadJoin.java

--- src/com/nishchay/thread/basic/join/Add100ThreadJoin.java
+++ src/com/nishchay/thread/basic/join/Add100ThreadJoin.java
@@ -1,24 +1,27 @@
 package com.nishchay.thread.basic.join;
 
+import java.util.stream.IntStream;
+
 public class Add100ThreadJoin {
 
     public static void main(String[] args) throws InterruptedException {
-
-        System.out.println("---------------Main started----------------");
-
-        Thread t1 = new Thread(new Sum100Task());
+        System.out.println("----Main started----");
+        Thread t1 = new Thread(() -> sum100());
         t1.start();
-
         t1.join();
+        System.out.println("----Main Ended----");
+    }
 
-        System.out.println("---------------Main Ended------------------");
+    private static void sum100() {
+        int sum = IntStream.rangeClosed(1, 100).sum();
+        System.out.println("sum of 100 = " + sum);
     }
 }
 
 /*
  * O/P =>
- * ---------------Main started----------------
+ * ----Main started----
  * sum of 100 = 5050
- * ---------------Main Ended------------------
+ * ----Main Ended----
  *
  * */
