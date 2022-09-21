33424f39169859e6a1aef09d1150d3c249044d83
Nishchay <nishchay.naresh@gmail.com>
2022-09-21 10:52:59 +0530

    +u concurrentpkg.pool.completionservice

--- src/com/nishchay/concurrentpkg/pool/completionservice/DownloadTask.java
+++ src/com/nishchay/concurrentpkg/pool/completionservice/DownloadTask.java
@@ -4,9 +4,8 @@ import java.util.concurrent.Callable;
 
 public class DownloadTask implements Callable<String> {
 
-    private String taskName;
-    private int sleepTime;
-
+    private final String taskName;
+    private final int sleepTime;
 
     public DownloadTask(String taskName, int sleepTime) {
         this.taskName = taskName;
@@ -18,7 +17,6 @@ public class DownloadTask implements Callable<String> {
         System.out.println("Started taskName: " + taskName);
         int size = sleepTime * 1000;
         Thread.sleep(size);
-        String res = "Completed taskName: " + taskName + " Downloaded data - " + size;
-        return res;
+        return "Completed taskName: " + taskName + " Downloaded data - " + size;
     }
 }
