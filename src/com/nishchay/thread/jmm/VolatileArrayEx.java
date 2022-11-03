4c3c3c4a5f046b6836ed316d0f497cc76ce85153
Nishchay <nishchay.naresh@gmail.com>
2022-11-03 13:21:35 +0530

    +u thread.jmm.VolatileArrayEx.java

--- src/com/nishchay/thread/jmm/VolatileArrayEx.java
+++ src/com/nishchay/thread/jmm/VolatileArrayEx.java
@@ -18,7 +18,7 @@ public class VolatileArrayEx {
         void clear();
     }
 
-    // Simple implementation using individual member
+    // Simple implementation using individual atomic member
     static class AlignedCountersA implements AlignmentTest.AlignedCounters {
         AtomicLong a = new AtomicLong(0);
         AtomicLong b = new AtomicLong(0);
@@ -46,7 +46,52 @@ public class VolatileArrayEx {
             a.set(0L);
             b.set(0L);
         }
+    }
 
+    // implementation using volatile array
+    static class AlignedCountersB implements AlignmentTest.AlignedCounters {
+
+        volatile AtomicLong[] counters;
+
+        AlignedCountersB() {
+            // write
+            counters = new AtomicLong[]{
+                    new AtomicLong(0),
+                    new AtomicLong(0)
+            };
+        }
+
+        @Override
+        public long get() {
+            // fetch/read
+            AtomicLong[] c = counters;
+            long valA = c[0].get();
+            long valB = c[1].get();
+            if (valA == valB) {
+                return valA;
+            } else {
+                throw new IllegalStateException((valA + " != " + valB));
+            }
+        }
+
+        @Override
+        public void increment() {
+            // fetch/read
+            AtomicLong[] c = counters;
+            c[0].incrementAndGet();
+            c[1].incrementAndGet();
+            // write has been taken care by Atomic variable
+            // for primitive array, need to another assignment of local variable array ref to attribute array ref
+        }
+
+        @Override
+        public void clear() {
+            // write
+            counters = new AtomicLong[]{
+                    new AtomicLong(0),
+                    new AtomicLong(0)
+            };
+        }
     }
 
 }
