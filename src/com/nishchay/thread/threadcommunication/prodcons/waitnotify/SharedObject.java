4ac97825ed6f3f0a11b253f05bcf00fd5449c7b7
Nishchay <nishchay.naresh@gmail.com>
2022-12-07 12:37:46 +0530

    +u thread.threadcommunication.prodcons.waitnotify.SharedObject.java

--- src/com/nishchay/thread/threadcommunication/prodcons/waitnotify/SharedObject.java
+++ src/com/nishchay/thread/threadcommunication/prodcons/waitnotify/SharedObject.java
@@ -5,9 +5,16 @@ public class SharedObject {
     private int data;
     private boolean isProdTurn = true;
 
+
+    /*
+    * Busy Spinning as Wait Strategy in Java:
+    *  Busy Spinning is a wait strategy  in which one thread waits for some condition to happen which is to be set by some other thread.
+    *  Here the waiting thread loops continuously without releasing the CPU cycles.
+    *
+    * */
     public synchronized void produce(int value) {
-        // why did we use while(busy == false) instead of if(busy == false)
-        // because of spurious wake ups
+        // why did we use while(busy == false) instead of if(busy == false) ?
+        // because of spurious wake-ups
         while (!isProdTurn) {
             try {
                 wait();
