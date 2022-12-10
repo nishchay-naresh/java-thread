f32b2212e26944c7092e6d3c4bd7be19c8087cb7
Nishchay <nishchay.naresh@gmail.com>
2022-12-10 23:18:14 +0530

    +u com.nishchay.Utils.java

--- src/com/nishchay/Utils.java
+++ src/com/nishchay/Utils.java
@@ -2,6 +2,11 @@ package com.nishchay;
 
 public class Utils {
 
+    /**
+     * A common util method to let wait current thread for the given object lock
+     *
+     * @param lockObject to keep the method as static , passing this(current objet) reference here
+     */
     public static void wait0(Object lockObject){
         try {
             lockObject.wait();
@@ -10,6 +15,11 @@ public class Utils {
         }
     }
 
+    /**
+     * A common util method to let current thread for given duration of millis
+     *
+     * @param millis int in the form of millisecond
+     */
     public static void sleep0(int millis){
         try {
             Thread.sleep(millis);
