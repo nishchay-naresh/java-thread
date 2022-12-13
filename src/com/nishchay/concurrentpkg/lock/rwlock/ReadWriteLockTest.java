94d5af5f6fc0767c51d6849e613ed1fdb8dc48d3
Nishchay <nishchay.naresh@gmail.com>
2022-12-13 11:53:01 +0530

    +u concurrentpkg.lock.rwlock

--- src/com/nishchay/concurrentpkg/lock/rwlock/ReadWriteLockTest.java
+++ src/com/nishchay/concurrentpkg/lock/rwlock/ReadWriteLockTest.java
@@ -2,28 +2,28 @@ package com.nishchay.concurrentpkg.lock.rwlock;
 
 /**
  * ReadWriteTest.java
- * Test program is to understanding ReentrantReadWriteLock concept
+ * Test program is to understand ReentrantReadWriteLock concept
  * Leveraging its two locks (read & write lock ) for get & out propose
  * Making non-thread safe collection ArrayList to ThreadSafe by guarding add() & get() method
  *
  */
 public class ReadWriteLockTest {
 
-    static final int READER_SIZE = 10;
-    static final int WRITER_SIZE = 4;
- 
+    private static final int WRITER_THREAD_COUNT = 4;
+    private static final int READER_THREAD_COUNT = 10;
+
     public static void main(String[] args) {
+
         Integer[] initialElements = {33, 28, 86, 99, 75};
- 
         ReadWriteList<Integer> sharedList = new ReadWriteList<>(initialElements);
  
-        for (int i = 0; i < WRITER_SIZE; i++) {
+        for (int i = 0; i < WRITER_THREAD_COUNT; i++) {
             new Writer(sharedList).start();
         }
  
-        for (int i = 0; i < READER_SIZE; i++) {
+        for (int i = 0; i < READER_THREAD_COUNT; i++) {
             new Reader(sharedList).start();
         }
- 
+
     }
 }
