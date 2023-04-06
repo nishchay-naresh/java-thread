4cc1739d58e06aab5cc5adbb184f1b434e080da5
Nishchay <nishchay.naresh@gmail.com>
2023-04-06 14:14:42 +0530

    +u thread.jmm.BarrierConditionExpose.java

--- src/com/nishchay/thread/jmm/BarrierConditionExpose.java
+++ src/com/nishchay/thread/jmm/BarrierConditionExpose.java
@@ -8,7 +8,15 @@ import static com.nishchay.Utils.sleep0;
 *   1. Make this done variable as volatile
 *   2. Or add a sleep(0), under the while loop
 *
-* TODO - Need a discussion around this.
+* ======== discussion around this ============
+*  why its working in both of the scenario :
+*  1. Make this done variable as volatile - done variable will always be read from main memory, so it is visible
+*  2. Or add a sleep(0), under the while loop - when we are putting sleep in executing thread, it will save the context in main memory
+*  so again done variable will be read from main memory, so it is visible
+*
+*  Without doing any of the above the thing : since done variable is a class variable it will be part of register in thread context.
+*  So when it gets modified by the other process, changes are not reflecting to register level.
+*
 *
 * */
 public class BarrierConditionExpose {
@@ -28,7 +36,7 @@ public class BarrierConditionExpose {
                     count++;
                     sleep0(0);
                 }
-                System.out.println("Exiting thread");
+                System.out.println("Exiting thread - " + count);
             }
         }).start();
 
@@ -40,3 +48,19 @@ public class BarrierConditionExpose {
     }
 
 }
+
+/*
+ * o/p =>
+ *
+ *	stuck in execution :
+ *	in main...
+ *	running ....
+ *	Setting done to true
+ *
+ *	Completing its execution :
+ *	in main...
+ *	running ....
+ *	Setting done to true
+ *	Exiting thread - 5444941
+ *
+ * */
